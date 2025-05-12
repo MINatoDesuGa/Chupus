2dbde343474b32dd94868a60e0845a3cf619b58a
MINatoDesuGa <lminlunsims14@gmail.com>
2025-05-12 12:07:40 +0530

    player input ui setup + player scripts added

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    public static GlobalVars Instance;
+
+    public RectTransform CanvasRectTransform;
+    private void Awake() {
+        if(Instance == null) { 
+            Instance = this; 
+        } else {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField] private GameObject _leftLeg;
+    [SerializeField] private GameObject _rightLeg;
+
+
+}
